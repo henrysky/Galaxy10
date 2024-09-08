df93c36fc0c58016ee53367eda0fd024017df3b6
Henry Leung <henryskyleung@gmail.com>
2024-09-08 15:08:11 -0400

    ruff format

--- downloader_gzdes_ab_des_dr8.py
+++ downloader_gzdes_ab_des_dr8.py
@@ -11,28 +11,38 @@ scale = 0.262
 height = 512
 width = 512
 
-iamge_dir = 'jpg_ab/'
-data_table = 'gz_decals_volunteers_ab.csv'
+iamge_dir = "jpg_ab/"
+data_table = "gz_decals_volunteers_ab.csv"
+
 
 def images_url(RA, DEC, scale, height, width, i):
-    str1 = 'https://www.legacysurvey.org/viewer/jpeg-cutout?layer=dr8&bands=grz'
-    str2 = '&ra={:f}&dec={:f}&pixscale={:f}&width={:d}&height={:d}'.format(RA[i], DEC[i], scale, height, width)
+    str1 = "https://www.legacysurvey.org/viewer/jpeg-cutout?layer=dr8&bands=grz"
+    str2 = "&ra={:f}&dec={:f}&pixscale={:f}&width={:d}&height={:d}".format(
+        RA[i], DEC[i], scale, height, width
+    )
     try:
-        urllib.request.urlretrieve(str1 + str2, iamge_dir + str(i).rjust(6, '0') + '.jpg')
+        urllib.request.urlretrieve(
+            str1 + str2, iamge_dir + str(i).rjust(6, "0") + ".jpg"
+        )
     except:
         pass
         print(i)
 
-radeg, decdeg = np.genfromtxt(data_table, skip_header=1, usecols=(1, 2), delimiter=",", unpack=True)
+
+radeg, decdeg = np.genfromtxt(
+    data_table, skip_header=1, usecols=(1, 2), delimiter=",", unpack=True
+)
+
 
 # Multi-threading here
 def main():
-    iterable = np.arange(0,len(radeg), 1)
+    iterable = np.arange(0, len(radeg), 1)
     pool = multiprocessing.Pool(8)
     func = partial(images_url, radeg, decdeg, scale, height, width)
     pool.map(func, iterable)
     pool.close()
     pool.join()
+
 
 if __name__ == "__main__":
     main()
